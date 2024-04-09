abc4e223202f50907d7935a1239af74a55766283
Nabila Zahra <nabilazehra1@gmail.com>
2024-04-09 20:38:21 +0500

    Update checkout.js

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,15 +1,9 @@
 var express = require("express");
 var router = express.Router();
-router.get("/",isLoggedIn,function(req,res){
+
+router.get("/",function(req,res){
 	res.render("cart/checkout",{user: req.user});
 });
 
-function isLoggedIn(req,res,next){
-	if(req.isAuthenticated()){
-		return next();
-	}
-	req.flash("error","Login to continue!!");
-	res.redirect("/login");
-}
 
 module.exports = router;
