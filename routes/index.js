a6583cc7209084d59b24d29be315efdbffe83435
Youshay <my07103@st.habib.edu.pk>
2024-04-21 19:54:16 +0500

    Fixes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var passport = require("passport");
 var User = require("../models/user");
 
 router.get("/",function(req,res){
-	res.redirect("/login");
+	res.redirect("/products");
 });
 
 router.get("/register",function(req,res){
